c78600050717c7d125f2327f0e2a089e0848e32f
Kaya Erol <kue202@nyu.edu>
2023-10-30 00:34:27 -0400

    Jo

--- sketch.js
+++ sketch.js
@@ -1,41 +1,103 @@
-// This is a creative coding project, The project details can be found here https://github.com/IDMNYU/CreativeCoding_FA23_SectionA_LadyK/blob/main/MidTermProject.md
-// The adjective I have chosen is Hopelessness. Because I want that's how I feel. 
+class Equation {
+  constructor() {
+    this.x = random(width);
+    this.y = random(-100, -10);
+    this.equation = this.getRandomEquation(); // Get a random engineering equation
+    this.speed = random(1, 5);
+  }
+
+  getRandomEquation() {
+    // Define an array of engineering equations, concepts, and algorithms
+    let equations = [
+      "E=mc^2",
+      "F=ma",
+      "ΔV=IR",
+      "∫(sin(x) dx) = -cos(x) + C",
+      "F=kx",
+      "Recursion",
+      "Hashtables",
+      "Bubble Sort",
+      "Depth First Search",
+      "Breadth First Search",
+      "Maxwell's Equations",
+      "Ohm's Law: V=IR",
+      "Newton's Laws",
+      "Thevenin's Theorem",
+      "Dijkstra's Algorithm",
+      "Quicksort",
+      "Binary Search",
+      "Kirchhoff's Laws",
+      "Laplace Transform",
+      "Fourier Transform",
+      "Navier-Stokes Equations",
+      "Finite Element Analysis",
+      "Amdahl's Law",
+      "Big O Notation",
+      "Euler's Method",
+      "Lagrangian Mechanics",
+      "Huffman Coding",
+      "Kalman Filter",
+      "Euler's Identity",
+      "Boolean Algebra",
+      "Principal Component Analysis",
+      "Bayes' Theorem",
+      "Moore's Law",
+      "P vs. NP Problem",
+      "Shannon's Information Theory",
+      "Gaussian Elimination",
+      "Game Theory",
+      "Monte Carlo Simulation",
+      "Algorithm Complexity",
+      "The Halting Problem",
+      "Turing Machines",
+      "Fractal Geometry",
+      "Einstein Field Equations",
+      "Stokes' Theorem",
+      "Pythagorean Theorem",
+      "A* Search Algorithm",
+      "Hooke's Law",
+      "Coulomb's Law",
+      "Schrodinger Equation",
+      "Heisenberg Uncertainty Principle",
+      // Add more equations or concepts here
+    ];
+  
+    // Return a random equation, concept, or algorithm from the array
+    return random(equations);
+  }
+
+  fall() {
+    this.y += this.speed;
+
+    // Reset equation if it goes off the screen
+    if (this.y > height) {
+      this.y = random(-100, -10);
+      this.x = random(width);
+      this.speed += 0.3; // Increase speed slightly over time
+      this.equation = this.getRandomEquation(); // Get a new random equation
+    }
+  }
+}
+
+let equations = [];
 
 function setup() {
   createCanvas(800, 800);
-  noFill();
-  stroke(255); // White lines
+  textSize(36);
 }
 
-let raindrops = [];
-
 function draw() {
-  background(0); // Black background
+  background(0);
 
-  // Create new raindrops
-  for (let i = 0; i < 10; i++) {
-    let x = random(width); // Random x position
-    let y = random(-100, -10); // Random y position above the canvas
-    let speed = random(5, 15); // Random falling speed
-    let len = random(10, 30); // Random line length
-    raindrops.push({ x, y, speed, len });
+  // Create new equations
+  if (random(1) < 0.1) {
+    equations.push(new Equation());
   }
 
-  // Code for house 
-  
-
-  // Update and display raindrops
-  for (let i = raindrops.length - 1; i >= 0; i--) {
-    let drop = raindrops[i];
-    line(drop.x, drop.y, drop.x, drop.y + drop.len); // Draw raindrop
-
-    drop.y += drop.speed; // Move raindrop down
-
-    // Remove raindrop if it goes below the canvas
-    if (drop.y > height) {
-      raindrops.splice(i, 1);
-    }
+  // Update and move equations
+  for (let equation of equations) {
+    equation.fall();
+    fill(255);
+    text(equation.equation, equation.x, equation.y);
   }
-
-
 }
